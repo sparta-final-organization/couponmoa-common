439de77ab4121550cfd10cfcf4435e38aa6d29f7
plusmate <psejin1478@gmail.com>
2025-04-22 15:08:06 +0900

    first commit

---
+++ src/main/java/com/couponmoa/backend/couponmoacommon/CouponmoaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmoaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponmoaCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couponmoa/backend/couponmoacommon/CouponmoaCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoacommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmoaCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
